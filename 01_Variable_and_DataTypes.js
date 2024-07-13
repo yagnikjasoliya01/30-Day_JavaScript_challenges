ab37e29a1e1f35e09174ea9a4d1e1d0300232606
Yagnik Jasoliya <146578487+yagnikjasoliya01@users.noreply.github.com>
2024-07-13 21:57:54 +0530

    Add files via upload

---
+++ 01_Variable_and_DataTypes.js
@@ -0,0 +1,39 @@
+//Activity - 1,2 and 3
+var first = 12;
+console.log(first);
+console.log(typeof first);
+
+var second = '12'
+console.log(second);
+console.log(typeof second);
+
+const third = true;
+console.log(third);
+console.log(typeof third);
+
+const obj = {
+    email:"yagnik@gmail.com",
+    name:"Yagnik",
+    arr: [2,4,3,2,46]
+}
+console.log(obj);
+console.log(typeof obj);
+
+let arr = [1,6,4,6,3,6];
+console.log(arr);
+console.log(typeof arr);
+
+//Activity 4
+let num = 34;
+console.log(num);
+num=30
+console.log(num);
+
+//Activity 5
+const num1 = 34;
+console.log(num);
+num1=30
+console.log(num);
+
+
+
