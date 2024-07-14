7dd613016ce63b704310a9f6445e606a74a2ceef
Yagnik Jasoliya <146578487+yagnikjasoliya01@users.noreply.github.com>
2024-07-14 07:08:37 +0530

    Update 01_Variable_and_DataTypes.js

--- 01_Variable_and_DataTypes.js
+++ 01_Variable_and_DataTypes.js
@@ -2,14 +2,17 @@
 var first = 12;
 console.log(first);
 console.log(typeof first);
+//int
 
 var second = '12'
 console.log(second);
 console.log(typeof second);
+//string
 
 const third = true;
 console.log(third);
 console.log(typeof third);
+//boolean
 
 const obj = {
     email:"yagnik@gmail.com",
@@ -18,10 +21,12 @@ const obj = {
 }
 console.log(obj);
 console.log(typeof obj);
+//object
 
 let arr = [1,6,4,6,3,6];
 console.log(arr);
 console.log(typeof arr);
+//object
 
 //Activity 4
 let num = 34;
@@ -31,6 +36,7 @@ console.log(num);
 
 //Activity 5
 const num1 = 34;
+//we can't change value of const variable
 console.log(num);
 num1=30
 console.log(num);
