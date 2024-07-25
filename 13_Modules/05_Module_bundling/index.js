432c944c324d5b7884a2bae4dc936ead43ab2e56
Yagnik Jasoliya <146578487+yagnikjasoliya01@users.noreply.github.com>
2024-07-25 21:28:30 +0530

    Add files via upload

---
+++ 13_Modules/05_Module_bundling/index.js
@@ -0,0 +1,5 @@
+import {greet} from './module1'
+import {add} from './module2'
+
+greet(Yagnik);
+console.log('sum of ' , add(3,5));
---
+++ 13_Modules/05_Module_bundling/module1.js
@@ -0,0 +1,8 @@
+//Task - 08
+//Use a module bundler like Webpack or Parcel to bundle multiple JavaScript files into a single file. Write a script to
+//demonstrate the bundling process.
+
+export function greet(name)
+{
+    console.log('hello my name is ${name}');//to install webpack localy - npm install webpack webpack-cli --save-dev
+}
